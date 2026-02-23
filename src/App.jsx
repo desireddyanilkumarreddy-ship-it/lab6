3f1620d983e9b2b19f52da8cf422cce32e678caa
Anil-2222 <desireddyanilkumarreddy@gmail.com>
2026-02-23 20:05:55 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
- 
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Always Welcome</h1>
+    <h1>Welcome to gh-pages</h1>
     </>
   )
 }
